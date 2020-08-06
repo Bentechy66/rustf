e03d7b94adc1867b07134ea85a45c98ad1324176
BenTechy66 <sendbenspam@yahoo.co.uk>
2020-08-06 02:32:22 +0100

    Start on Ctf type

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-rustf
+# RUSTF
+*Rust library to interact with an RACTF CTF*
+
+**Currently under development**
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use reqwest::blocking::Client;
+use serde::{Serialize,Deserialize};
+
+pub struct Ctf {
+    http_client: Client,
+    api_url: String,
+}
+
+impl Ctf {
+    pub fn new(api_url: String) -> Ctf {
+        let version_url = format!("{}/stats/version/", &api_url);
+
+        let construction = Ctf {
+            http_client: Client::new(),
+            api_url: api_url.to_string(),
+        };
+
+        // TODO: Check the API endpoint actually exists and returns valid JSON
+        let body = construction.http_client.get(&version_url).send()
+
+        construction
     }
 }
