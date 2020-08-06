3b5ce15610d2511b1bcd7d565be18cca3236b2ac
BenTechy66 <sendbenspam@yahoo.co.uk>
2020-08-06 02:46:25 +0100

    Update a thing

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@ pub struct Ctf {
 
 impl Ctf {
     pub fn new(api_url: String) -> Ctf {
-        let version_url = format!("{}/stats/version/", &api_url);
-
         let construction = Ctf {
             http_client: Client::new(),
-            api_url: api_url.to_string(),
+            api_url: api_url,
         };
+
+        let version_url = format!("{}/stats/version/", &construction.api_url);
 
         // TODO: Check the API endpoint actually exists and returns valid JSON
         let body = construction.http_client.get(&version_url).send();
